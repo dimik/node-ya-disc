321ebb7ffa9f314ae85f902c3b662c4a2cf3ca9d
dimik <dimik@yandex.ru>
2013-09-07 17:09:16 +0700

    calMethod -> request

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ var url = require('url'),
     disc = new YaDisc({ token: 'c5a90c24062242889b23459a0c15ff53' });
 
 http.createServer(function (request, response) {
-    console.log(request.url, request.headers, request.method);
-    var options = url.parse(request.url),
-        query = querystring.parse(options.query),
-        apiMethod = options.pathname.match(/\w+/)[0];
 
-    disc.callMethod(apiMethod, query)
+    console.log(request.url, request.headers, request.method);
+
+    var options = url.parse(request.url),
+        args = querystring.parse(options.query),
+        method = options.pathname.match(/\w+/)[0];
+
+    disc.request(method, args)
         .then(function (res) {
-            response.writeHead(200, {'Content-Type': 'application/json'});
+            response.writeHead(200, {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'});
+            console.log('JSON: ', res.toJSON());
             response.end(res.toJSON());
         }, function (err) {
             console.log(err);
